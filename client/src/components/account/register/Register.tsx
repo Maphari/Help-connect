9d13a0806b048fc3b2275269310d4af959f4988c
Tino1017 <tmagobeya1@gmail.com>
2023-10-03 15:55:49 +0200

    updated registration-page

--- client/src/components/account/register/Register.tsx
+++ client/src/components/account/register/Register.tsx
@@ -18,75 +18,74 @@ export const Register: React.FC = () => {
   const [confirmPasswordError, setConfirmPasswordError] = useState<string>("");
 
   const usernameValidationHandler: (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => void = function (e: React.ChangeEvent<HTMLInputElement>) {
-    const usernameRegex: RegExp = /^[a-zA-Z0-9_]*$/;
-
-    if (!usernameRegex.test(username.trim())) {
+  ) => void = function () {
+  
+    if (username.trim().length < 5) {
       setUsernameError("Please enter a valid username");
     } else {
       setUsernameError("");
-      setUsername(e.target.value);
     }
   };
 
   const emailValidatorHandler: (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => void = function (e: React.ChangeEvent<HTMLInputElement>) {
+    
+  ) => void = function () {
     const emailRegex: RegExp =
       /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (!emailRegex.test(email.trim())) {
+    if (!emailRegex.test(email.trim()) || !email) {
       setEmailError("Please enter valid email");
     } else {
       setEmailError("");
-      setEmail(e.target.value);
     }
   };
 
   const mobileNumberValidatorHandler: (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => void = function (e: React.ChangeEvent<HTMLInputElement>) {
-    if (mobileNumber.length < 10 || Number(mobileNumber.trim())) {
+  ) => void = function () {
+    if (mobileNumber.length < 10 || Number(mobileNumber.trim()) || !mobileNumber) {
       setMobileNumberError("Please enter valid mobile number");
     } else {
       setMobileNumberError("");
-      setMobileNumber(e.target.value);
     }
   };
 
   const passwordValidatorHandler: (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => void = function (e: React.ChangeEvent<HTMLInputElement>) {
+  ) => void = function () {
     const passwordRegex: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-    if (!passwordRegex.test(password.trim())) {
+    if (!passwordRegex.test(password) || !password) {
       setPasswordError("Password must be more than 8 characters");
     } else {
       setPasswordError("");
-      setPassword(e.target.value);
     }
   };
 
   const confirmPasswordValidatorHandler: (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => void = function (e: React.ChangeEvent<HTMLInputElement>) {
-    const passwordRegex: RegExp = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  ) => void = function () {
 
-    if (!passwordRegex.test(password.trim())) {
-      setConfirmPasswordError("Password must be more than 8 characters");
-    } else if (password.trim() !== confirmPassword.trim()) {
-      setConfirmPasswordError("Password doesn't match");
-    } else {
+    if (password === confirmPassword || !confirmPassword) {
       setConfirmPasswordError("");
-      setConfirmPassword(e.target.value);
+    } else {
+      setConfirmPasswordError("Password must be more than 8 characters");
     }
   };
+
+
+  const submitDataHandler: (e: React.FormEvent<HTMLFormElement>) => void = function(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    try {
+      if(!usernameError || !emailError || !mobileNumberError || !passwordError || !confirmPasswordError)
+        console.log(`everything went well ${username}, ${email}, ${mobileNumber}, ${password}, ${confirmPassword}`);
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <>
       <section className="register-container rounded">
-        <form className="py-7 px-7 rounded-xl drop-shadow-xl">
+        <form className="py-7 px-7 rounded-xl drop-shadow-xl" onSubmit={(e) => submitDataHandler(e)}>
           <h1 className="text-2xl mb-1 font-bold text-blue-950">
             Register account
           </h1>
@@ -97,12 +96,15 @@ export const Register: React.FC = () => {
             <label className="text-sm opacity-50 mb-1" htmlFor="username">
               Username
             </label>
-            <div className="flex items-center w-full">
+            <div className={`flex items-center w-full border ${usernameError && 'border-red-500'} rounded`}>
               <span className="px-2  py-[10px] bg-slate-100 rounded-l text-slate-500">
                 <Profile />
               </span>
               <input
-                onChange={(e) => usernameValidationHandler(e)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setUsername(e.target.value)
+                  usernameValidationHandler()
+                }}
                 value={username}
                 id="username"
                 type="text"
@@ -115,12 +117,15 @@ export const Register: React.FC = () => {
             <label className="text-sm opacity-50 mb-1" htmlFor="email">
               Email
             </label>
-            <div className="flex items-center w-full">
+            <div className={`flex items-center ${emailError && 'border-red-500'} w-full`}>
               <span className="px-2  py-[10px] bg-slate-100 rounded-l text-slate-500">
                 <Mail />
               </span>
               <input
-                onChange={(e) => emailValidatorHandler(e)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setEmail(e.target.value)
+                  emailValidatorHandler()
+                }}
                 value={email}
                 id="email"
                 type="email"
@@ -133,12 +138,15 @@ export const Register: React.FC = () => {
             <label className="text-sm opacity-50 mb-1" htmlFor="mobile">
               Mobile Number
             </label>
-            <div className="flex items-center w-full">
+            <div className={`flex items-center w-full ${mobileNumberError && 'border-red-500'}`}>
               <span className="px-2 py-[11px] bg-slate-100 rounded-l text-slate-500 text-sm">
                 <TelIcon />
               </span>
               <input
-                onChange={(e) => mobileNumberValidatorHandler(e)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setMobileNumber(e.target.value)
+                  mobileNumberValidatorHandler()
+                }}
                 value={mobileNumber}
                 id="mobile"
                 type="tel"
@@ -151,12 +159,15 @@ export const Register: React.FC = () => {
             <label className="text-sm opacity-50 mb-1" htmlFor="password">
               Password
             </label>
-            <div className="flex items-center w-full">
+            <div className={`flex items-center w-full ${passwordError && 'border-red-500'}`}>
               <span className="px-2 py-[11px] bg-slate-100 rounded-l text-slate-500 text-sm">
                 <LockIcon />
               </span>
               <input
-                onChange={(e) => passwordValidatorHandler(e)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setPassword(e.target.value)
+                  passwordValidatorHandler()
+                }}
                 value={password}
                 id="password"
                 type="password"
@@ -172,12 +183,15 @@ export const Register: React.FC = () => {
             >
               Confirm Password
             </label>
-            <div className="flex items-center w-full">
+            <div className={`flex items-center w-full ${confirmPasswordError && 'border-red-500'}`}>
               <span className="px-2 py-[11px] bg-slate-100 rounded-l text-slate-500 text-sm">
                 <LockIcon />
               </span>
               <input
-                onChange={(e) => confirmPasswordValidatorHandler(e)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setConfirmPassword(e.target.value)
+                  confirmPasswordValidatorHandler()
+                }}
                 value={confirmPassword}
                 id="confirm-password"
                 type="password"
